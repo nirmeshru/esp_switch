b6bb38a0cd3c8140d131279dd19d8eebeb4ca8ea
nirmeshru <nirmesh.singh@gmail.com>
2021-09-06 20:00:36 +0530

    two switch button example

---
+++ main/app_priv.h
@@ -0,0 +1,20 @@
+/*
+   This example code is in the Public Domain (or CC0 licensed, at your option.)
+
+   Unless required by applicable law or agreed to in writing, this
+   software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+   CONDITIONS OF ANY KIND, either express or implied.
+*/
+#pragma once
+#include <stdint.h>
+#include <stdbool.h>
+
+#define DEFAULT_POWER  true
+extern esp_rmaker_device_t *switch_device1;
+extern esp_rmaker_device_t *switch_device2;
+void app_driver_init(void);
+int app_driver_set_state(bool state);
+int app_driver_set_state2(bool state);
+bool app_driver_get_state(void);
+void app_driver_init1(void);
+void app_driver_init2(void);
